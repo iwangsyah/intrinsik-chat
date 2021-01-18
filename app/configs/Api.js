5596780f68c85b50f5b2998ccf9fd06e08a63d2c
Wahyu Irwansyah <wahyuiwangsyah@gmail.com>
2021-01-18 14:59:09 +0700

    change endpoint

--- app/configs/Api.js
+++ app/configs/Api.js
@@ -1,6 +1,6 @@
 const BASE_URL =
-  __DEV__ ? 'http://localhost:3000' :
-    'https://obscure-temple-13039.herokuapp.com';
+  // __DEV__ ? 'http://localhost:3000' :
+  'https://obscure-temple-13039.herokuapp.com';
 
 export default (Api = {
   URL_LOGIN: `${BASE_URL}/login`,
